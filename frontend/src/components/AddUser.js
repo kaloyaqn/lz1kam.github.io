7ca12c14b3109ef580e80894f7643e26047d086e
Kaloyan Angelov <kallnoob11@gmail.com>
2023-01-02 17:16:35 +0200

    create fullstack

---
+++ frontend/src/components/AddUser.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+ 
+const AddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [mqsto, setMqsto] = useState("");
+  const navigate = useNavigate();
+ 
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name,
+        email,
+        gender,
+        mqsto,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+  return (
+    <div className="columns mt-5">
+      <div className="column is-half">
+        <form onSubmit={saveUser}>
+          <div className="field">
+            <label className="label">Име</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Mqsto</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={mqsto}
+                onChange={(e) => setMqsto(e.target.value)}
+                placeholder="Mqsto"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Имейл</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Пол</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Запази
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+ 
+export default AddUser;
